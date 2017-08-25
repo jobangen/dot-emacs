7fc7def2089883b22a4e079e3a8eb03dfcfd7dee
Jan Ole Bangen <jobangen@gmail.com>
2017-08-25 21:40:51 +0200

    fix: init link melpa

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
 (setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
-                         ("melpa"     . "https://melpa.milkbox.net/packages/")
+                         ("melpa"     . "https://melpa.org/packages/")
                          ("org"       . "http://orgmode.org/elpa/")))
 (package-initialize)
 (setq package-enable-at-startup nil)
