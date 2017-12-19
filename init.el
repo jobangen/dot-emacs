f2f37d3eabb11875dffdeab5c5fdac4dd33a831f
Jan Ole Bangen <jobangen@gmail.com>
2017-12-19 15:04:28 +0100

    fix init: structure

--- init.el
+++ init.el
@@ -1,27 +1,34 @@
+;;; package
 (require 'package)
 (setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
                          ("melpa"     . "https://melpa.org/packages/")))
 (package-initialize)
 (setq package-enable-at-startup nil)
 
+;;; load-paths
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/use-package"))
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/etc"))
 
+;;; use-package
 (eval-when-compile
   (require 'use-package))
 (require 'bind-key)
-
 (use-package diminish
   :ensure t)
 
+;;; custom-file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;; EXWM
+(use-package dot-exwm
+  :ensure exwm)
+
+;;; org-mode
 (use-package org
   :load-path ("site-lisp/org-mode/lisp"
               "site-lisp/org-mode/contrib/lisp"))
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
 (org-babel-load-file "~/.emacs.d/myinit.org")
 
 
-(put 'narrow-to-region 'disabled nil)
-(put 'erase-buffer 'disabled nil)
