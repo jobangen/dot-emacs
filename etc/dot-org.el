d19b4fdd6654b6b8666d98be81d7cabb7851086c
Jan Ole Bangen <jobangen@gmail.com>
2017-12-26 00:03:13 +0100

    add dot-org (part)

---
+++ etc/dot-org.el
@@ -0,0 +1,18 @@
+;;;_,dot-org
+
+(require 'org)
+
+
+
+;;; org-agenda
+
+(setq org-agenda-diary-file "journal.org")
+(setq org-agenda-include-diary nil)
+
+(provide 'dot-org)
+;;; dot-org.el ends here
+
+
+
+
+
