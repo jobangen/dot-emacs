785caf044b92438a2a3110c512016d4e843d43c8
Jan Ole Bangen <jobangen@gmail.com>
2018-01-09 01:11:39 +0100

    add pkg archives: org

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 ;;;
 (require 'package)
 (setq package-archives '(("gnu"       . "https://elpa.gnu.org/packages/")
-                         ("melpa"     . "https://melpa.org/packages/")))
+                         ("melpa"     . "https://melpa.org/packages/")
+                         ("org"       . "http://orgmode.org/elpa/")))
 (package-initialize)
 (setq package-enable-at-startup nil)
 
