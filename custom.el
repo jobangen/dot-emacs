da504c450f1a286ad5697b7bcefa8fde66278f64
Jan Ole Bangen <jobangen@gmail.com>
2018-02-05 12:58:02 +0100

    custom: fringe

--- custom.el
+++ custom.el
@@ -70,4 +70,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "#f3f3f3"))))
  '(org-mode-line-clock ((t (:foreground "red" :box (:line-width -1 :style released-button))))))
