581c5d0c7df963a97166802ce1df0ec7093c745a
Jan Ole Bangen <jobangen@gmail.com>
2018-03-19 17:05:42 +0100

    add auctex: fa/add-latex-acronym

--- etc/dot-auctex.el
+++ etc/dot-auctex.el
@@ -138,6 +138,29 @@
   '(define-key TeX-mode-map "\""
      'job/enquote-wrapper))
 
+;; https://florian.adamsky.it/2018/03/09/emacs-add-acronyms.html
+;;;###autoload
+(defun fa/add-latex-acronym (region-beg region-end)
+  "This function reads the written out form of an acronym via the
+minibuffer and adds it to the acronym list in a latex
+document. Addtionally, it sorts all acronyms in the list."
+  (interactive "r")
+  (save-excursion
+    (let ((acronym
+           (if (region-active-p)
+               (buffer-substring region-beg region-end)
+             (read-from-minibuffer "Acronym: ")))
+          (full-name (read-from-minibuffer "Full Name: ")))
+      (beginning-of-buffer)
+      (if (search-forward "\\begin{acronym}" nil t)
+          (progn
+            (deactivate-mark)
+            (open-line 1)
+            (forward-line 1)
+            (insert (concat "  \\acro{" acronym "}{" full-name "}"))
+            (beginning-of-line)
+            (sort-lines nil (point) (search-forward "\\end{acronym}" nil nil)))
+        (user-error "No acronym environment found")))))
 
 (provide 'dot-auctex)
 ;;; dot-auctex.el ends here
