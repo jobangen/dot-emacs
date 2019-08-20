b491e4a6e6037edfcccb5fe04335e6906dc14425
Jan Ole Bangen <jobangen@gmail.com>
2019-08-20 11:15:07 +0200

    add telephone-line

--- init.el
+++ init.el
@@ -1739,6 +1739,17 @@ rotate entire document."
   :bind ("C-s" . swiper-isearch))
 
 ;;; T
+(use-package telephone-line
+  :config
+  (setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+        telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+        telephone-line-primary-right-separator 'telephone-line-cubed-right
+        telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+  (setq telephone-line-height 20
+        telephone-line-evil-use-short-tag t)
+
+  (telephone-line-mode 1))
+
 (use-package tile
   :bind ("C-c w" . imalison:hydra-tile/body)
   :config
