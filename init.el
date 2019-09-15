a74aa5c54aa677f58af148c44cad474a84445465
Jan Ole Bangen <jobangen@gmail.com>
2019-09-15 19:15:43 +0200

    add dumb-junp

--- init.el
+++ init.el
@@ -765,6 +765,16 @@
               ("TAB" . dired-subtree-toggle)
               ("<tab>" . dired-subtree-toggle)))
 
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g b" . dumb-jump-back)
+         ("M-g q" . dumb-jump-quick-look)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy))
+
 ;;; E
 (use-package edbi)
 
