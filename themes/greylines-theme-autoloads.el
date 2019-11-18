3a1f5ee18779ce0d0713ae32da9274264e1c72db
Jan Ole Bangen <jobangen@gmail.com>
2019-11-18 11:42:03 +0100

    fix greylines-theme, comments

--- themes/greylines-theme-autoloads.el
+++ themes/greylines-theme-autoloads.el
@@ -1,11 +1,11 @@
-;;; leuven-theme-autoloads.el --- automatically extracted autoloads
+;;; greylines-theme-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
 
 
 ;;;### (autoloads nil "greylines-theme" "greylines-theme.el" (21716 58293
 ;;;;;;  419088 556000))
-;;; Generated autoloads from leuven-theme.el
+;;; Generated autoloads from greylines-theme.el
 
 (when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
