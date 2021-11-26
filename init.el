3102a961af9f86bad929dbb07ffd856e27614a11
Jan Ole Bangen <jobangen@gmail.com>
2021-11-26 12:39:21 +0100

    multiple cursors, rm kbd

--- init.el
+++ init.el
@@ -1795,7 +1795,7 @@ of a BibTeX field into the template. Fork."
 
 (use-package multiple-cursors
   :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C-<" . mc/mark-next-like-this)
+         ;; ("C-<" . mc/mark-next-like-this)
          ("C-c C-<" . mc/mark-all-like-this))
   :diminish multiple-cursors)
 
