de2583b877a4dbb628c3181bb5a9f14d039baa49
Jan Ole Bangen <jobangen@gmail.com>
2021-11-28 17:43:31 +0100

    fix dumb-jump

--- init.el
+++ init.el
@@ -838,13 +838,13 @@
 
 (use-package dumb-jump
   :bind (("M-g o" . dumb-jump-go-other-window)
-         ("M-g j" . dumb-jump-go)
-         ("M-g b" . dumb-jump-back)
          ("M-g q" . dumb-jump-quick-look)
          ("M-g i" . dumb-jump-go-prompt)
          ("M-g x" . dumb-jump-go-prefer-external)
          ("M-g z" . dumb-jump-go-prefer-external-other-window))
-  :config (setq dumb-jump-selector 'ivy))
+  :config
+  (setq dumb-jump-selector 'ivy)
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
 ;;; E
 (use-package edbi)
