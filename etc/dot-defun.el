aee5f8f61c03d13209c7c2a32bbbc81b827bf4bb
Jan Ole Bangen <jobangen@gmail.com>
2025-03-30 22:35:59 +0200

    update wt

--- etc/dot-defun.el
+++ etc/dot-defun.el
@@ -704,7 +704,7 @@ count."
   (interactive)
   (async-start-process
    "wt"
-   "C:/Program Files/WindowsApps/Microsoft.WindowsTerminal_1.21.10351.0_x64__8wekyb3d8bbwe/wt.exe"
+   "C:/Program Files/WindowsApps/Microsoft.WindowsTerminal_1.22.10731.0_x64__8wekyb3d8bbwe/wt.exe"
    nil
    "-d" (replace-regexp-in-string "/" "\\" (expand-file-name default-directory) t t)))
 
