70a44f45097d97f3ee33af268f80f68421418556
Jan Ole Bangen <jobangen@gmail.com>
2025-08-21 10:17:37 +0200

    add dired sidebar

--- init.el
+++ init.el
@@ -1022,6 +1022,24 @@
 
   )
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "  ")
+  ;; (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
 (use-package dired-subtree
   :bind (:map dired-mode-map
               ("TAB" . dired-subtree-toggle)
